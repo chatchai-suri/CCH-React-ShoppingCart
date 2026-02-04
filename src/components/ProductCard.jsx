de5979181257fa7e5bc30dcf641bedc431533b01
chatchai <chsuriyawan@yahoo.com>
2026-02-04 14:57:51 +0700

    ProductCard: assigned props

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,32 @@
-import _React from 'react'
+import _React, { useState } from "react";
 
-function ProductCard() {
+function ProductCard(props) {
+  // ------ JS area ------
+  const {
+    productItem: { id, category, description, title, images, price, rating },
+  } = props;
+
+  // State
+  const [showDesc, setShowDesc] = useState(false)
+
+  // ------ JSX area ------
   return (
-    <div>ProductCard</div>
-  )
+    <div className="card bg-base-100 w-70 shadow-xl pt-2">
+      <p className="ps-4">{id}</p>
+      <figure className="h-40 mt-2">
+        <img src={images} alt={title} className="h-full w-full object-contain"/>
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{title}</h2>
+        <p>{category}</p>
+        <p className={`${showDesc ? '' : 'line-clamp-2'} cursor-pointer hover:text-blue-600 transition-colors `} onClick={()=>setShowDesc(prev=>!prev)} >{description}</p>
+        <p>{price}</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ProductCard
+export default ProductCard;
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,20 +1,23 @@
-import {_useState} from "react";
+import { _useState } from "react";
 import ProductCard from "./ProductCard";
 
 function ProductList(props) {
   // ------ JS area ------
-  const {products} = props
+  const { products } = props;
+  console.log(products[0]);
   // State
-  
 
   // Function
 
   // ------ JSX area ------
   return (
     <div className="w-2/3 bg-gray-300">
-      <h2>Product</h2>
-      <ProductCard />
-      <pre>{JSON.stringify(products, null, 2)}</pre>
+      <h2>Product List</h2>
+      <div className="flex flex-col gap-2 ps-2">
+        {products.map((el) => (
+          <ProductCard key={el.id} productItem={el} />
+        ))}
+      </div>
     </div>
   );
 }
