2df37d95a053b9a0f85c08f37ddff50b747bfb41
chatchai <chsuriyawan@yahoo.com>
2026-02-05 14:42:38 +0700

    CartSummary: format adjust

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -7,12 +7,12 @@ function CartItem(props) {
   // ------ JSX area ------
   return (
     <div className="flex justify-between p-2 text-sm g-2">
-      <p className="flex-1 line-clamp-1 min-w-0 ">{title.split(' ')[0]+ " " +title.split(' ')[1]}</p>
-      <div className="flex gap-1 items-center justify-center w-20">
+      <p className="flex-1 line-clamp-1 min-w-1/2 ">{title.split(' ')[0]+ " " +title.split(' ')[1]}</p>
+      <div className="flex gap-1 items-center justify-center w-fit">
         <p className="border w-5 h-5 flex justify-center items-center cursor-pointer hover:bg-gray-500" onClick={()=>decQuantity(id)} >-</p>
         <p className="border w-5 h-5 flex justify-center items-center cursor-pointer hover:bg-gray-500" onClick={()=>incQuantity(id)}>+</p>
       </div>
-      <p className="w-24 text-right font-mono font-semibold">{quantity} * ฿{price}</p>
+      <p className="flex-1 text-end font-mono font-semibold">{quantity} * ฿{price.toLocaleString(undefined, { minimumFractionDigits: 2 })}</p>
     </div>
   );
 }
--- src/components/CartSummary.jsx
+++ src/components/CartSummary.jsx
@@ -7,46 +7,79 @@ function CartSummary(props) {
 
   // Function
   // itemsPrice is sum of all item price x quantity
-  const itemsPrice = carts.reduce((acc, el)=> (acc + el.price * el.quantity),0)
+  const itemsPrice = carts.reduce((acc, el) => acc + el.price * el.quantity, 0);
 
   // assume tax rate = 7%
-  const tax = itemsPrice * 0.07
+  const tax = itemsPrice * 0.07;
 
   // shipping free where itemsPrice >= 500
-  const shipping = itemsPrice > 500 || itemsPrice === 0 ? 0 : 50
+  const shipping = itemsPrice > 500 || itemsPrice === 0 ? 0 : 50;
 
   // Total sum of ItemsPrice + tax + shipping fee
-  const total = itemsPrice + tax + shipping
+  const total = itemsPrice + tax + shipping;
 
   // ------ JSX area ------
   return (
     <div className="w-1/3 bg-orange-100 p-2">
       <h2 className="text-2xl p-2">Cart Items:</h2>
       {carts.length === 0 && <p>Cart is Empty</p>}
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-0.5">
         {carts.map((el) => (
-          <CartItem key={el.id} item={el} decQuantity={decQuantity} incQuantity={incQuantity} />
+          <CartItem
+            key={el.id}
+            item={el}
+            decQuantity={decQuantity}
+            incQuantity={incQuantity}
+          />
         ))}
       </div>
-      <div className="flex flex-col gap-0.5 text-sm mt-4 border-t border-white ">
-        <div className="flex justify-between items-center p-2 ">
-          <span>Items Price</span>
-          <span>฿{itemsPrice.toLocaleString("Th-TH", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
-        </div>
-        <div className="flex justify-between items-center p-2 ">
-       <span>Tax</span>
-          <span>฿{tax.toLocaleString("Th-TH", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
-        </div>
-        <div className="flex justify-between items-center p-2 ">
-<span>Shipping</span>
-          <span>฿{shipping.toLocaleString("Th-TH", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
-        </div>
-        <div className="flex justify-between items-center p-2 font-bold ">
-          <span>Total</span>
-          <span className="font-bold text-right w-20 border ">฿{total.toLocaleString("Th-TH", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
-        </div>
-      </div>
-      {/* <pre>{JSON.stringify(carts, null, 2)}</pre> */}
+      {carts.length > 0 && (
+        <>
+          <div className="divider">Total</div>
+          <div className="flex flex-col gap-0.5 text-sm ">
+            <div className="flex justify-between items-center px-2 ">
+              <span>Items Price</span>
+              <span>
+                ฿
+                {itemsPrice.toLocaleString("Th-TH", {
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2,
+                })}
+              </span>
+            </div>
+            <div className="flex justify-between items-center p-2 ">
+              <span>Vat</span>
+              <span>
+                ฿
+                {tax.toLocaleString("Th-TH", {
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2,
+                })}
+              </span>
+            </div>
+            <div className="flex justify-between items-center p-2 ">
+              <span>Shipping</span>
+              <span>
+                ฿
+                {shipping.toLocaleString("Th-TH", {
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2,
+                })}
+              </span>
+            </div>
+            <div className="flex justify-between items-center p-2 font-bold text-lg ">
+              <span>Total</span>
+              <span className="border-b-4 border-double border-current px-1">
+                ฿
+                {total.toLocaleString("Th-TH", {
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2,
+                })}
+              </span>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
